a46f4cfef5658a62c7d9aad8afaa57aa89168a59
Paul Mainwood <paul.mainwood@gmail.com>
2022-05-19 14:56:09 +0100

    Latest source from gz file and new script

--- old scripts/variant_script.r
+++ old scripts/variant_script.r
@@ -4,28 +4,32 @@ library(magrittr)
 library(dplyr)
 library(data.table)
 
+#Get case data for the three we care about
+#Note because of the lack of permanent links you have to go to https://sars2.cvr.gla.ac.uk/cog-uk/, select each and paste your session ID into the link manually.
+
+ba4 <- read.csv("https://sars2.cvr.gla.ac.uk/cog-uk/session/7259368f6ff02dff15c2880a0787dbf8/download/downloadConcern?w=")
+ba5 <- read.csv("https://sars2.cvr.gla.ac.uk/cog-uk/session/7259368f6ff02dff15c2880a0787dbf8/download/downloadConcern?w=")
+#This one takes ages
+ba2 <- read.csv("https://sars2.cvr.gla.ac.uk/cog-uk/session/7dd010c5c0ce6874fa970f96f49ede69/download/downloadConcern?w=")
+
 first_date <- as.Date("2022-04-10")
 last_date <- as.Date("2022-06-05")
+occurrences <- function(df, start_date = "2022-04-10"){
+  a <- df %>% count(sample_date) %>% filter(sample_date >= as.Date(start_date))
+  return(a)
+}
 
-#Get case data - this takes ages 
-all_data <- fread("https://cog-uk.s3.climb.ac.uk/phylogenetics/latest/cog_metadata.csv.gz")
-our_data <- select(filter(all_data, sample_date >= first_date), lineage, sample_date)
-our_data <- mutate(our_data, lineage = substr(lineage, 1, 4))
-
-grp = group_by(our_data, sample_date, lineage) %>% summarise(number = n())
-
-a = select(filter(grp, lineage == "BA.2"), sample_date, number)
-b = select(filter(grp, lineage == "BA.4"), sample_date, number)
-c = select(filter(grp, lineage == "BA.5"), sample_date, number)
-
+a = occurrences(ba2)
+b = occurrences(ba4)
+c = occurrences(ba5)
 
 df <- purrr::reduce(list(a,b,c), dplyr::left_join, by = 'sample_date')
 colnames(df) = c("sample_date", "ba2", "ba4", "ba5")
 df <- mutate(df, ba4_frac = ba4 / (ba2 + ba4 + ba5))
 df <- mutate(df, ba5_frac = ba5 / (ba2 + ba4 + ba5))
 
-regba4 <- lm(log2(ba4_frac) ~ date(sample_date), data = df)
-#regba5 <- lm(log2(ba5_frac) ~ date(sample_date), data = df)
+regba4 <- lm(log2(df$ba4_frac) ~ date(df$sample_date))
+#regba5 <- lm(log2(df$ba5_frac) ~ date(df$sample_date))
 
 plot(date(df$sample_date), log2(df$ba4_frac), col = 2, ylim = c(-13, 0), xlim = c(first_date, last_date), yaxt = "n", xaxt = "n", ylab = "Share of total, % - log2 scale", xlab = "Date", main = "BA.4 as a proportion of COG-UK sequences")
 #points(date(df$sample_date), log2(df$ba5_frac), col = 3)
@@ -40,3 +44,4 @@ abline(h = 0, col = "gray", lty= "dotted")
 abline(h = -1, col = "gray", lty= "dashed")
 title(sub="Plot of COG-UK data, by @paulmainwood, inspired by previous work from @alexselby1770", adj=1, line=4, font=1, cex.sub = 0.6)
 
+ba4 <- fread("https://cog-uk.s3.climb.ac.uk/phylogenetics/latest/cog_metadata.csv.gz")
