d4f9e24cdd29ab5a96466a082564b88b8469b89d
Paul Mainwood <paul.mainwood@gmail.com>
2022-08-19 07:18:01 +0100

    Weekly switch and weighting colourisation

--- variant_script proportions vs baseline variant_ggplot2.R
+++ variant_script proportions vs baseline variant_ggplot2.R
@@ -6,6 +6,7 @@ library(tidyverse)
 library(magrittr)
 library(data.table)
 library(lubridate)
+conflict_prefer("filter", "dplyr")
 
 first_date <- as.Date("2022-06-15")
 last_date <- as.Date("2022-08-22")
@@ -20,18 +21,24 @@ grp = group_by(our_data, sample_date, lineage) %>% summarise(number = n())
 #Function to take new and base variant names and produce dataframes of one as 
 #fraction of the other, and regress the log of the proportions by date to give 
 #a best-fit line.
-proportions_new_variant <- function(new_variant_name, base_variant_name, grp) {
+proportions_new_variant <- function(new_variant_name, base_variant_name, grp, aggregate_by_week_threshold = 2, aggregate_by_week_days = 21) {
   
   base <- grp %>% filter(str_detect(lineage, base_variant_name)) %>% summarise(number = sum(number)) %>% select(sample_date, number)
   new <- grp %>% filter(str_detect(lineage, new_variant_name)) %>% summarise(number = sum(number)) %>% select(sample_date, number)
+  
   
   #Collect counts of each lineage in weighting by total of BA sequences
   df <- purrr::reduce(list(base, new), dplyr::left_join, by = 'sample_date')
   colnames(df) = c("sample_date", "base", "new")
 
+  #Aggregate up by week if less than
+  if (sum(tail(df$new, aggregate_by_week_days), na.rm = T) / aggregate_by_week_days < aggregate_by_week_threshold){
+    df <- df %>% group_by(sample_date = cut(sample_date, "week")) %>% summarise(base = sum(base, na.rm =T), new = sum(new, na.rm = T))
+  }
+  
   df <- mutate(df, new_frac = new / base, new_frac_log2 = log2(new / base), new_variant_name = new_variant_name, base_variant_name = base_variant_name)
   wts <- coalesce(1/(1/df$base + 1/df$new), 0)
-  df <- mutate(df, wts = wts)
+  df <- mutate(df, wts = wts/sum(wts) * dim(df)[1])
 
   #Regression with log2 transformation
   reg <- lm(log2(new_frac) ~ date(sample_date), weights = wts, data = df)
@@ -39,13 +46,12 @@ proportions_new_variant <- function(new_variant_name, base_variant_name, grp) {
   return(list(df = df, reg = reg))
   }
 
-
 #Use the function to grab whichever variants and base you want and plot. Manual
 #awkwardness here, since when automate, get weird visuals.
 
 base_variant = "BA.5.2"
-new_variants = c("BA.4.6", "BA.5.1", "BF.5", "BA.2.75")
-dfs <- sapply(new_variants, function(x) proportions_new_variant(x, base_variant, grp))
+new_variants = c("BA.4.6", "BA.5.1", "BA.2.75", "BE.1", "BF.3")
+dfs <- sapply(new_variants, function(x) proportions_new_variant(x, base_variant, grp, aggregate_by_week_threshold = 2))
 df <- bind_rows(dfs[1, ])
 
 intercepts <- sapply(seq(1:length(dfs[2,])), function(x) dfs[2,][[x]]$coefficients[[1]])
@@ -54,7 +60,7 @@ ablines <- tibble(intercept = intercepts, slope = slopes, cols = new_variants)
 
 
 ggplot(df, aes(x = sample_date, y = new_frac_log2, color = new_variant_name)) + 
-  geom_point(size = 3, aes(alpha = base)) + 
+  geom_point(size = 3, aes(alpha = wts)) + 
   scale_alpha(guide = "none") +
   scale_x_date(date_breaks = "1 week", date_labels = "%d %b", limits = c(first_date, last_date)) + 
   xlab("Sample date") +
@@ -65,3 +71,4 @@ ggplot(df, aes(x = sample_date, y = new_frac_log2, color = new_variant_name)) +
   geom_abline(data = ablines, aes(intercept = intercept, slope = slope, colour = cols)) +
   labs(title = paste0("Selected variant frequency in COG-UK data as share of ", base_variant, " and subvariants"), caption = "Plot of COG-UK data, by @paulmainwood, based on earlier work from @alexselby1770. Regression weighted by inverse probability of selection; last few data points based on very small values, shown by fading") +
   theme(plot.caption = element_text(size = 7), plot.title = element_text(size = 16, hjust = 0.5), legend.position = c(0.94, 0.1), legend.title=element_blank())
+
